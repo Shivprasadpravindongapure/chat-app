81c15852c6acef34b11d7e148b36f37e26adc2cc
Shivprasad Pravin Dongapure <prasaddongapure7660@gmail.com>
2026-02-01 03:27:31 +0530

    Fix critical import path error in socket auth middleware
    - Correct import path from './env.js' to '../lib/env.js'
    - This was causing deployment failure on Render

--- backend/src/middleware/socket.auth.middleware.js
+++ backend/src/middleware/socket.auth.middleware.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-import { ENV } from "./env.js";
+import { ENV } from "../lib/env.js";
 
 export const socketAuthMiddleware = async (socket, next) => {
   try {
